ec8e60fb4bc01465d6615b2644519bb6718ffd05
alessio <alessiodg99.adg@gmail.com>
2021-03-03 19:14:45 +0100

    fine footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
 @endsection
 
 @section('content')
-<div id="site_content">
+<div>
     <div class="card_wrapper d-flex">
         <div class="card">
             <img src="{{asset('img/fumetto_batman.jpg')}}" alt="">
@@ -195,4 +195,71 @@
     </section>
     <!-- /.buy -->
 </div>
+@endsection
+
+@section('footer')
+<div class="container">
+    <div class="list_wrapper">
+        <ul>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+        </ul>
+        <ul>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+        </ul>
+        <ul>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+            <li>lorem</li>
+        </ul>
+    </div>
+    <ul>
+        <li>lorem</li>
+        <li>lorem</li>
+        <li>lorem</li>
+    </ul>
+    <div class="copyright">
+    Lorem ipsum dolor sit <span><a href="#">amet consectetur</a></span> adipisicing elit. Ipsa, quae incidunt? <span><a href="#">Labore sapiente architecto</a></span> nemo libero perferendis, dolor quis ex.
+</div>
+</div>
+<!-- /container -->
+<div class="logo">
+    <img src="{{ asset('img/logo-background-v2.png')}}" alt="">
+</div>
+<!-- /logo -->
+<nav class="nav nav-bottom d-flex">
+    <div class="sign-up">
+        sign-up now !
+    </div>
+    <div class="social d-flex">
+        <span>follow us</span>
+        <ul>
+            <li><i class="fab fa-facebook-f"></i></li>
+            <li><i class="fab fa-youtube"></i></li>
+            <li><i class="fab fa-twitter" aria-hidden="true"></i></li>
+            <li><i class="fab fa-pinterest" aria-hidden="true"></i></li>
+            <li><i class="fas fa-map-marker-alt"></i></li>
+        </ul>
+    </div>
+</nav>
+<!-- /nav-bottom -->
+
 @endsection
